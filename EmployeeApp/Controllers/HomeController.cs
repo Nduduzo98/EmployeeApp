de474295fef59cf2fc02732f7d6e428bfc14f537
unknown <ngobesenduduzo080@gmail.com>
2022-11-27 23:42:15 +0200

    initial commit

---
+++ EmployeeApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        employeedbEntities empEntity = new employeedbEntities();
+        public ActionResult Index()
+        {
+            var listOfData = empEntity.Employees.ToList();
+            return View(listOfData);
+        }
+        
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Create(Employee employee)
+        {
+
+            empEntity.Employees.Add(employee);
+            empEntity.SaveChanges();
+            ViewBag.Message = "Data Saved Successfully";
+
+            return View();
+        }
+
+
+    }
+}
